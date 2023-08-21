894640f259511a3249f4fd522d098b94b146ae86
Mateusz Ledwon <mateusz.ledwon97@gmail.com>
2023-08-21 20:02:45 +0200

    feat: implement transaction interfaces

---
+++ internal/data/adapter/pg_transaction.go
@@ -0,0 +1,63 @@
+package adapter
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/LazyCodeTeam/just-code-backend/internal/core/port"
+	"github.com/LazyCodeTeam/just-code-backend/internal/data/db"
+)
+
+type PgTransactionFactory struct {
+	db *pgxpool.Pool
+}
+
+func NewPgTransactionFactory(db *pgxpool.Pool) *PgTransactionFactory {
+	return &PgTransactionFactory{db: db}
+}
+
+func (f *PgTransactionFactory) Begin(ctx context.Context) (port.Transaction, error) {
+	tx, err := f.db.Begin(ctx)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to begin transaction: %v", "err", err)
+
+		return nil, err
+	}
+	queries := db.New(tx)
+
+	return &PgTransaction{tx: tx, queries: queries}, nil
+}
+
+type PgTransaction struct {
+	tx      pgx.Tx
+	queries *db.Queries
+}
+
+func (t *PgTransaction) ContentRepository(ctx context.Context) port.ContentRepository {
+	return NewPgContentRepository(t.queries)
+}
+
+func (t *PgTransaction) Commit(ctx context.Context) error {
+	err := t.tx.Commit(ctx)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to commit transaction: %v", "err", err)
+
+		return err
+	}
+
+	return nil
+}
+
+func (t *PgTransaction) Rollback(ctx context.Context) error {
+	err := t.tx.Rollback(ctx)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to rollback transaction: %v", "err", err)
+
+		return err
+	}
+
+	return nil
+}
